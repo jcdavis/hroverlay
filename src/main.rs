c457f3094e171ea1f349ce4b65123bfd943c207e
Jackson Davis <jackson@jcdav.is>
2021-01-01 21:48:33 -0800

    Comments + cleanup

--- src/main.rs
+++ src/main.rs
@@ -49,8 +49,9 @@ mod basic_app_ui {
     use super::*;
     use std::cell::RefCell;
     use std::ops::Deref;
-    use nwg::{ControlBase, NwgError};
+    use nwg::{ControlBase, NwgError, HTextAlign};
     use winapi::um::winuser::{WS_CLIPCHILDREN, WS_VISIBLE, WS_EX_TOPMOST, WS_EX_LAYERED, WS_POPUP, SetLayeredWindowAttributes, LWA_COLORKEY};
+    use winapi::um::wingdi::RGB;
 
     pub struct HRViewerUi {
         inner: Rc<HRViewer>,
@@ -69,13 +70,14 @@ mod basic_app_ui {
                 .ex_flags(WS_EX_TOPMOST | WS_EX_LAYERED)
                 .flags(WS_POPUP | WS_VISIBLE)
                 .size((100, 100))
+                //TODO: figure out how to not hardcode this
                 .position((1820, 1100))
                 .build()?;
 
             nwg::Font::builder()
                 .family("Arial")
                 .size(50)
-                .weight(5)
+                .weight(700)
                 .build(&mut data.font)?;
 
             nwg::TextInput::builder()
@@ -83,6 +85,7 @@ mod basic_app_ui {
                 .size((100, 120))
                 .parent(&data.window)
                 .font(Some(&data.font))
+                .align(HTextAlign::Right)
                 .background_color(Some([255, 0, 0]))
                 .readonly(true)
                 .build(&mut data.hr)?;
@@ -131,7 +134,7 @@ mod basic_app_ui {
             match ui.window.handle {
                 nwg::ControlHandle::Hwnd(hwnd) => {
                     unsafe {
-                        SetLayeredWindowAttributes(hwnd, 0x000000FF, 0, LWA_COLORKEY);
+                        SetLayeredWindowAttributes(hwnd, RGB(255, 0, 0), 0, LWA_COLORKEY);
                     }
                 }
                 _ => {
@@ -174,6 +177,8 @@ fn create_bt_updater() -> Result<(), &'static str> {
     }).ok_or("Couldn't find HRM")?;
 
     ohr.connect().map_err(|_e| "Couldn't connect")?;
+    // 0x2A37 is the heart rate measurement characteristic
+    // 00000000-0000-1000-8000-00805F9B34FB is the base 128 bit bluetooth UUID
     let mut bytes: [u8; 16] = [0x00,0x00,0x2A,0x37,0x00,0x00,0x10,0x00,0x80,0x00,0x00,0x80,0x5F,0x9B,0x34,0xFB];
     bytes.reverse();
     let uuid = UUID::B128(bytes);
@@ -192,6 +197,7 @@ fn create_bt_updater() -> Result<(), &'static str> {
     Ok(())
 }
 
+// For basic UI testing. Just throws a bunch of data in the 0-199 range as a placeholder
 fn create_dummy_updater() {
     thread::spawn(|| {
         let mut count: i64 = 0;
